1148216b73601ddaf804f45827048f51ffc1dfb6
antonesp <97282336+antonesp@users.noreply.github.com>
2022-01-18 11:46:58 +0100

    Add files via upload

--- genPhantom.m
+++ genPhantom.m
@@ -1,5 +1,21 @@
+% Funktionen har til at kalde 'phantom' kommandoen og gennere et billede
+% med en given størrelse.
+
+%Dato 18-01-2022
+%Af Anton Espholm og Clara Hollenbeck
+
+%Input størrelsen af billedet
+%Output et billede som kan gemmes.
 
 function phan = genPhantom(x)
+
+
+% Input chekkes.
+assert(isempty(x),'Fejl du har ikke andgivet en ønsket billede størrelse')
+% x afroundes og eventuelle complexe eller negative tal fjernes.
+x = floor(abs(x));
+assert(isstring(x),'Fejl x må ikke være en string')
+assert(isinteger(int8(x))||x<=0,'Fejl x skal være et heltal over 0')
 
 P = phantom('Modified Shepp-Logan',x);
 
