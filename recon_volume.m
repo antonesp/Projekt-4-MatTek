8fadf7c3b2dc1313d392074aa5aecec64333474e
ClaraHol <97283088+ClaraHol@users.noreply.github.com>
2022-01-18 14:14:14 +0100

    Add files via upload

--- recon_volume.m
+++ recon_volume.m
@@ -9,12 +9,12 @@ function volume = recon_volume(M,vek)
 %Redigeret af Clara Hollenbeck 18-1-2022
 
 assert(length(size(M))==3,'Input skal være et 3D array')
-assert(isvector(vek),'Input 2 skal være en vektor')
+
 
 [x,y,z]=size(M);        %Udregner størrelsen af arrayet
 
 
-if isempty(vek)        %Tjekker om vek er tom
+if ~exist('vek')        %Tjekker om vek existerer
     V = zeros(x,y,z);  %Matrix med samme størrelse som M, fyldt med 0'er
    for i=1:z
         f=(M(:,:,i));  %Tager et udsnit af M
@@ -24,6 +24,7 @@ if isempty(vek)        %Tjekker om vek er tom
     V(:,:,i)=invLim;             %Indsætter det i V
    end
 else
+    assert(isvector(vek),'Input 2 skal være en vektor') %Tjekker at vek er en vektor
     l=length(vek);               %Længden af vek
     
     %Laver en matrix med samme nxm som M,og samme dybde som længden af vek, fyldt
